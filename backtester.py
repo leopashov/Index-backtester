4f76f643901d9d2ed171cbf986d456c151f4f923
leo <lp916@ic.ac.uk>
2022-12-13 15:33:03 +0200

    working graphs

--- backtester.py
+++ backtester.py
@@ -41,21 +41,66 @@ def getEqualSplitHoldings(_prices, holdRs):
     equalSplitHoldings = (0.5*holdRs.ETH) + (0.5*holdRs.BTC)
     return equalSplitHoldings
 
+def writeRemainderOfRow(array, row, prices):
+    array[row, 2] = array[row, 0] * prices.ETH[row]
+    array[row, 3] = array[row, 1] * prices.BTC[row]
+    array[row, 4] = array[row, 3] + array[row, 2]
+    array[row, 5] = array[row, 3] / array[row, 4]
 
-def main():
-    prices = createPricesDataFrame()
 
-    fig1 = px.line(prices, title="eth-usd and btc-usd price history")
+def getRebalancedReturns(priceData, targetBtcWeight, rebalanceFrequency):
+    # create numpy array of length price data (rows correspond to dates)
+    # using numpy as will have to iterate through
+    a = np.zeros((len(priceData), 6))
+    # Columns: 0.EthHoldings, 1.BtcHoldings, 2.EthUsdValue, 3.BtcUsdVal, 4.TotalValue, 5.BTCProportion
+    a[0, 0] = (1-targetBtcWeight) / priceData.ETH[0]
+    a[0, 1] = targetBtcWeight / priceData.BTC[0]
+    writeRemainderOfRow(a, 0, priceData)
+
+    for i in range(1,len(a)):
+        if i % rebalanceFrequency == 0:
+            #rebalance
+            btcValue = a[i-1, 1] * priceData.BTC[i]
+            ethValue = a[i-1, 0] * priceData.ETH[i]
+            totalValue = btcValue + ethValue
+            btcWeight = btcValue/totalValue
+            btcToSell = ((btcWeight-targetBtcWeight)*totalValue)/priceData.BTC[i]
+            ethToBuy = ((btcWeight-targetBtcWeight)*totalValue)/priceData.ETH[i]
+            a[i, 1] = a[i-1, 1] - btcToSell
+            a[i, 0] = a[i-1, 0] + ethToBuy
+            writeRemainderOfRow(a, i, priceData)
+        else:
+            # eth and btc holdings stay same as prev step
+            a[i, 0] = a[i-1, 0]
+            a[i, 1] = a[i-1, 1]
+            writeRemainderOfRow(a, i, priceData)
+    # np.savetxt("rebalanced.csv", a, delimiter=",")
+    return(a)
+
+def displayFigures(priceData, hold, equalHold, rebal, btcTarget, rebalanceFreq):
+    fig1 = px.line(priceData, title="eth-usd and btc-usd price history")
     fig1.show()
 
-    holdReturns = getHoldReturns(prices)
-    equalHoldReturns = getEqualSplitHoldings(prices, holdReturns)
-    print(holdReturns)
-    fig2 = px.line(holdReturns, title = 'hold returns')
-    fig2.add_trace(go.Scatter(x = equalHoldReturns.index, y = equalHoldReturns, mode="lines", name = '50/50 hold', showlegend = True))
+    fig2Title = 'returns - BtcWeight: {weight}, rebalance frequency: {freq}'.format(weight = btcTarget, freq = rebalanceFreq)
+    print(fig2Title)
+    fig2 = px.line(hold, title = fig2Title)
+    fig2.add_trace(go.Scatter(x = equalHold.index, y = equalHold, mode="lines", name = '50/50 hold', showlegend = True))
+    print(rebal[:, 4])
+    fig2.add_trace(go.Scatter(x = equalHold.index, y = rebal[:, 4], mode="lines", name = 'Rebalanced', showlegend = True))
     fig2.show()
 
     
+
+def main():
+    targetBtcWeight = 0.001
+    rebalanceFrequency = 90 #days
+    prices = createPricesDataFrame()
+
+    holdReturns = getHoldReturns(prices)
+    equalHoldReturns = getEqualSplitHoldings(prices, holdReturns)
+    rebalArray = getRebalancedReturns(prices, targetBtcWeight, rebalanceFrequency)
+
+    displayFigures(prices, holdReturns, equalHoldReturns, rebalArray, targetBtcWeight, rebalanceFrequency)    
 
 
 if __name__ == "__main__":
--- import.py
+++ import.py
@@ -34,46 +34,49 @@ def createPricesDataFrame():
 def main():
 
     dfPrices = createPricesDataFrame()
+    dfPrices.dropna(inplace=True)
     print(dfPrices)
-#     # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
-#     REBALANCEFREQ = 1 # how regularly portfolio is balanced, measured in days
-#     TARGETBTCPROPORTION = 0.5
-#     # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+    REBALANCEFREQ = 1 # how regularly portfolio is balanced, measured in days
+    TARGETBTCPROPORTION = 0.5
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
     
-#     # priceFig = px.line(dfPrices, x='Date', y=['EthOpen', 'BTCOpen'], title="Eth and BTC prices")
-#     # priceFig.show()
-#     dfPrices.at[0,"EthInPortfolio"] = (1-TARGETBTCPROPORTION)/(dfPrices.at[0, "EthOpen"])
-#     dfPrices.at[0,"BTCInPortfolio"] = TARGETBTCPROPORTION/(dfPrices.at[0, "BTCOpen"])
-#     dfPrices["EthHODLValue"] = 2 * (dfPrices["EthOpen"]) * (dfPrices.at[0, "EthInPortfolio"])
-#     dfPrices["BTCHODLValue"] = 2 * (dfPrices["BTCOpen"]) * (dfPrices.at[0, "BTCInPortfolio"])
-#     dfPrices["ETH+BTCHODLValue"] = (dfPrices["EthHODLValue"] + dfPrices['BTCHODLValue'])/2
-#     hodlFig = px.line(dfPrices, x = "Date", y=['EthHODLValue', 'BTCHODLValue', 'ETH+BTCHODLValue'], title="HODL comparisons")
-#     hodlFig.show()
+    # priceFig = px.line(dfPrices, x='Date', y=['EthOpen', 'BTCOpen'], title="Eth and BTC prices")
+    # priceFig.show()
+    
+    dfPrices.at['2015-08-07','EthInPortfolio'] = (1-TARGETBTCPROPORTION)/(dfPrices.at['2015-08-07', "EthOpen"])
+    dfPrices.at['2015-08-07','BTCInPortfolio'] = TARGETBTCPROPORTION/(dfPrices.at['2015-08-07', "BTCOpen"])
+    dfPrices["EthHODLValue"] = 2 * (dfPrices["EthOpen"]) * (dfPrices.at['2015-08-07', "EthInPortfolio"])
+    dfPrices["BTCHODLValue"] = 2 * (dfPrices["BTCOpen"]) * (dfPrices.at['2015-08-07', "BTCInPortfolio"])
+    dfPrices["ETH+BTCHODLValue"] = (dfPrices["EthHODLValue"] + dfPrices['BTCHODLValue'])/2
+    hodlFig = px.line(dfPrices, x = dfPrices.index, y=['EthHODLValue', 'BTCHODLValue', 'ETH+BTCHODLValue'], title="HODL comparisons")
+    hodlFig.show()
     
     
-#     dfPrices["Eth+Btc EqualWeight"] = 0
-#     for index, row in enumerate(dfPrices.index):
-#         if index > 0:
-#             if index % REBALANCEFREQ == 0:
-#                 # rebalance eth and btc holdings
-#                 BTCDollarValue = dfPrices.at[row-1, 'BTCInPortfolio'] * dfPrices.at[row, 'BTCOpen']
-#                 EthDollarValue = dfPrices.at[row-1, 'EthInPortfolio'] * dfPrices.at[row, 'EthOpen']
-#                 portfolioValue = BTCDollarValue + EthDollarValue
-#                 BTCProportion = BTCDollarValue/portfolioValue
-#                 dfPrices.at[row, 'BTCInPortfolio'] = dfPrices.at[row-1, 'BTCInPortfolio'] - (((BTCProportion-TARGETBTCPROPORTION)*portfolioValue)/dfPrices.at[row, 'BTCOpen'])
-#                 dfPrices.at[row, 'EthInPortfolio'] = dfPrices.at[row-1, 'EthInPortfolio'] + (((BTCProportion-TARGETBTCPROPORTION)*portfolioValue)/dfPrices.at[row, 'EthOpen'])
-#             else:
-#                 dfPrices.at[row, 'BTCInPortfolio'] = dfPrices.at[row-1, 'BTCInPortfolio']
-#                 dfPrices.at[row, 'EthInPortfolio'] = dfPrices.at[row-1, 'EthInPortfolio']
-#     dfPrices['indexValue'] = (dfPrices['EthInPortfolio'] * dfPrices['EthOpen']) + dfPrices['BTCInPortfolio'] * dfPrices['BTCOpen']
-#     dfPrices['BTCWeight'] = dfPrices['BTCInPortfolio'] * dfPrices['BTCOpen'] / dfPrices['indexValue']
-#     dfPrices['ETHWeight'] = dfPrices['EthInPortfolio'] * dfPrices['EthOpen'] / dfPrices['indexValue']
-#     dfPrices['BTCScaledWeight'] = 2000*dfPrices['BTCWeight']
+    dfPrices["Eth+Btc EqualWeight"] = 0
+    for index, row in enumerate(dfPrices.index):
+        if index > 0:
+            if index % REBALANCEFREQ == 0:
+                # rebalance eth and btc holdings
+                BTCDollarValue = dfPrices.at[dfPrices.index[index-1], 'BTCInPortfolio'] * dfPrices.at[dfPrices.index[index], 'BTCOpen']
+                EthDollarValue = dfPrices.at[dfPrices.index[index-1], 'EthInPortfolio'] * dfPrices.at[dfPrices.index[index], 'EthOpen']
+                portfolioValue = BTCDollarValue + EthDollarValue
+                BTCProportion = BTCDollarValue/portfolioValue
+                dfPrices.at[dfPrices.index[index], 'BTCInPortfolio'] = dfPrices.at[dfPrices.index[index-1], 'BTCInPortfolio'] - (((BTCProportion-TARGETBTCPROPORTION)*portfolioValue)/dfPrices.at[dfPrices.index[index], 'BTCOpen'])
+                dfPrices.at[dfPrices.index[index], 'EthInPortfolio'] = dfPrices.at[dfPrices.index[index-1], 'EthInPortfolio'] + (((BTCProportion-TARGETBTCPROPORTION)*portfolioValue)/dfPrices.at[dfPrices.index[index], 'EthOpen'])
+            else:
+                dfPrices.at[dfPrices.index[index], 'BTCInPortfolio'] = dfPrices.at[dfPrices.index[index-1], 'BTCInPortfolio']
+                dfPrices.at[dfPrices.index[index], 'EthInPortfolio'] = dfPrices.at[dfPrices.index[index-1], 'EthInPortfolio']
+    dfPrices['indexValue'] = (dfPrices['EthInPortfolio'] * dfPrices['EthOpen']) + dfPrices['BTCInPortfolio'] * dfPrices['BTCOpen']
+    dfPrices['BTCWeight'] = dfPrices['BTCInPortfolio'] * dfPrices['BTCOpen'] / dfPrices['indexValue']
+    dfPrices['ETHWeight'] = dfPrices['EthInPortfolio'] * dfPrices['EthOpen'] / dfPrices['indexValue']
+    dfPrices['BTCScaledWeight'] = 2000*dfPrices['BTCWeight']
 
-#         # print(dfPrices["EthHODLValue"][row])
-#     print(dfPrices[['BTCInPortfolio', 'EthInPortfolio', 'indexValue', 'BTCWeight', 'ETHWeight']])
-#     stratFig = px.line(dfPrices, x = "Date", y=['EthHODLValue', 'BTCHODLValue', 'ETH+BTCHODLValue', 'indexValue', 'BTCScaledWeight'], title="Strategy comparisons")
-#     stratFig.show()
+        # print(dfPrices["EthHODLValue"][index])
+    print(dfPrices[['BTCInPortfolio', 'EthInPortfolio', 'indexValue', 'BTCWeight', 'ETHWeight']])
+    stratFig = px.line(dfPrices, x = dfPrices.index, y=['EthHODLValue', 'BTCHODLValue', 'ETH+BTCHODLValue', 'indexValue', 'BTCScaledWeight'], title="Strategy comparisons")
+    stratFig.show()
+    dfPrices.to_csv('dataFrame.csv')
 
 
 if __name__ == "__main__":
